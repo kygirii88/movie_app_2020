b34feefa7626327f10d99419e15f14b5898d1427
kygirii88 <kygirii88@gmail.com>
2020-08-17 01:35:37 +0900

    add genres

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from "./Movie";
+import "./App.css"
 
 class App extends React.Component{
     state =  {
@@ -7,18 +9,42 @@ class App extends React.Component{
         movies: [],
     };
     getMovies = async () => {
-        const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+        //const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+        /** 위 내용을 아래와 같이 바꿀 수 있음 : ES6 */
+        const {data: {data : {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+        console.log("get movie data :>>", movies);
+        this.setState({movies, isLoading : false}) ;
     }
     componentDidMount() {
         this.getMovies();
     }
 
     render() {
-        const { isLoading } = this.state;
+        const { isLoading, movies } = this.state;
     return (
-        <div>
-            {isLoading ? "Loading..." : "We are ready"}
-        </div>
+        <section className={"container"}>
+            {isLoading
+                ? (
+                    <div className={"loader"}>
+                        <span className={"loader_text"}>Loading...</span>
+                    </div>
+                )
+                : ( /** html css */
+                    <div className={"movies"}>
+                        {movies.map(movie => (
+                         <Movie
+                             key={movie.id}
+                             id={movie.id}
+                             year={movie.year}
+                             title={movie.title}
+                             summary={movie.summary}
+                             poster={movie.medium_cover_image}
+                             genres={movie.genres}
+                         />
+                         ))}
+                    </div>
+                )}
+        </section>
     )
   }
 }
